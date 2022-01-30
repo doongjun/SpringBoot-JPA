5384686f3e939cda2d3da1071cf595bb82aebef7
doongjun <doongjun.kim@gmail.com>
2022-01-30 23:32:29 +0900

    Feat: 양방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -23,12 +24,21 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            team.addMember(member);
+
+//          em.flush();
+//          em.clear();
+
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+
+            System.out.println("===================");
+            for(Member m : members) {
+                System.out.println("m :: " + m.getId());
+            }
+            System.out.println("===================");
 
             tx.commit();
         } catch (Exception e) {
