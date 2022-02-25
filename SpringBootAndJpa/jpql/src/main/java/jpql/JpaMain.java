180629c11209ae7a81f3ac30f2ada5daa288e10c
doongjun <doongjun.kim@gmail.com>
2022-02-25 22:44:18 +0900

    Feat: jpql 기본 문법

---
+++ SpringBootAndJpa/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,43 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+
+            // 다건 조회
+            List<Member> resultList = query1.getResultList();
+            // 단건 조회
+            Member result = query1.getSingleResult();
+
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
