95658902cc29937e97603e4e90b8aef1c0c133b8
doongjun <doongjun@kakao.com>
2022-03-04 22:27:03 +0900

    Feat: 페치 조인 결과 확인

--- ex3-jpql/src/main/java/jpql/JpaMain.java
+++ ex3-jpql/src/main/java/jpql/JpaMain.java
@@ -15,29 +15,40 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
             member1.setUsername("member1");
-            member1.setTeam(team);
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("member2");
-            member2.setTeam(team);
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("member2");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username from Team t join t.members m";
+            //String query = "select m from Member m"; // 지연로딩
+            String query = "select m from Member m join fetch m.team";
 
-            List<String> result = em.createQuery(query, String.class)
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            for(String s : result) {
-                System.out.println("s = " + s);
+            for(Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", teamName = " + member.getTeam().getName());
             }
 
             tx.commit();
